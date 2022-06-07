0084bff385e6c339cd4f1f7f682dc84cb466b042
Tingyeu Yang <ytingyeu@asu.edu>
2022-06-07 16:58:52 -0700

    refactor insert

--- Tree/OrderStatisticsTree/OrderStatisticsTree.py
+++ Tree/OrderStatisticsTree/OrderStatisticsTree.py
@@ -16,7 +16,7 @@ class OSTreeNode(object):
         self.size = 1
 
 
-class OSTree(object):
+class OSTree:
     """
     Order Statistic Tree
 
@@ -24,39 +24,46 @@ class OSTree(object):
     Assume the values of its nodes are unique, i.e. no mutiple nodes share the same value.
     """
 
-    def __init__(self, root: OSTreeNode = None) -> None:
-        self.root = root
+    def __init__(self) -> None:
+        self._root = None
 
-    def insert(self, new_node: OSTreeNode) -> None:
+    def insert(self, insert_val: int) -> None:
 
-        def _insert_node(root: OSTreeNode, new_node: OSTreeNode):
-            if root.val > new_node.val:
-                if not root.left:
-                    root.left = new_node
+        def _insert_rec(root: OSTreeNode, insert_val: int) -> OSTreeNode:
+            if not root:
+                return OSTreeNode(insert_val)
 
-                else:
-                    _insert_node(root.left, new_node)
-
+            if insert_val < root.val:
+                root.left = _insert_rec(root.left, insert_val)
             else:
-
-                if not root.right:
-                    root.right = new_node
-
-                else:
-                    _insert_node(root.right, new_node)
+                root.right = _insert_rec(root.right, insert_val)
 
             # size of the currnet node increase one after insert
             root.size += 1
 
-        _insert_node(self.root, new_node)
+            return root
+
+        if not self._root:
+            self._root = OSTreeNode(insert_val)
+
+        else:
+            self._root = _insert_rec(self._root, insert_val)
 
     def delete(self, target: int) -> None:
+        def _find_inorder_successor(curr: OSTreeNode) -> OSTreeNode:
+            curr = curr.right
 
-        def _delete_node(target: int, descendants_need_fix: List) -> bool:
+            while curr.left:
+                curr = curr.left
+            return curr
 
-            curr = self.root
+        def _delete_rec(target: int, descendants_need_fix: List) -> bool:
+
+            curr = self._root
             parent = None
 
+            # find the target node and also record the nodes on the path
+            # will use this list to maintain node size after deletion
             while curr and curr.val != target:
                 parent = curr
                 descendants_need_fix.append(parent)
@@ -68,53 +75,55 @@ class OSTree(object):
                     curr = curr.right
 
             if not curr:
-                print(f'Delete failed: {target} not found.')
                 return False
 
-            # Node to be delted has no child
+            # case: target node has no child
             if not curr.left and not curr.right:
-                # if deleted node is not root
-                if curr != self.root:
+
+                # if the target node is not the root of the tree then simply delete it
+                if curr != self._root:
                     if parent.left == curr:
                         parent.left = None
                     else:
                         parent.right = None
                 else:
-                    self.root = None
+                    self._root = None
 
-            # Node to be deleted has two children
+            # case: target node has two children
             elif curr.left and curr.right:
+
                 # Find inorder successor or predecessor of the node
                 # here we use successor
-                successor = self._find_inorder_successor(curr)
+                successor = _find_inorder_successor(curr)
 
                 # store successor value
                 successor_val = successor.val
 
                 # Recursively delete the successor.
                 # Note that the successor has at most one child (right child)
-                _delete_node(successor.val, descendants_need_fix)
+                _delete_rec(successor.val, descendants_need_fix)
 
                 # copy value of the successor to the current node
                 curr.val = successor_val
 
-            # Node to be deleted has one child:
+            # case: target node has one child:
             else:
-                # Copy the child to the node and delete the child
+                # back up the pointer to the child
                 if curr.left:
                     child = curr.left
                 else:
                     child = curr.right
 
-                # if deleted node is not root
-                if curr != self.root:
+                # if the target node is not root then replace it with the child node
+                # we should not just replace the value because the chlid might have grandchildren
+                if curr != self._root:
                     if curr == parent.left:
                         parent.left = child
 
                     else:
                         parent.right = child
                 else:
-                    self.root = child
+                    self._root = child
 
             return True
 
@@ -122,19 +131,14 @@ class OSTree(object):
         # The size of these nodes are impacted because of node deletion.
         descendants_need_fix = []
 
-        if _delete_node(target, descendants_need_fix):
+        if _delete_rec(target, descendants_need_fix):
             print(f'Delete {target}')
             print('Nodes to be fixed: ', end='')
             print(' '.join(str(item.val) for item in descendants_need_fix))
 
-            self._fix_all_sum(self.root, descendants_need_fix)
-
-    def _find_inorder_successor(self, curr: OSTreeNode) -> OSTreeNode:
-        curr = curr.right
-
-        while curr.left:
-            curr = curr.left
-        return curr
+            self._fix_all_sum(self._root, descendants_need_fix)
+        else:
+            print(f'Delete failed: {target} not found.')
 
     def _fix_all_sum(self, root: OSTreeNode, descendants_need_fix: List[int]) -> None:
         """Maintain size of each tree node after insert, delete, or value change. Start from the passed root.
@@ -192,7 +196,7 @@ class OSTree(object):
             else:
                 return _select_kth_node(root.right, k - left_size)
 
-        return _select_kth_node(self.root, k)
+        return _select_kth_node(self._root, k)
 
     def get_rank(self, target: int) -> int:
         """Find the rank of the node with a specific value.
@@ -206,7 +210,7 @@ class OSTree(object):
             int: the rank of the target node.
         """
 
-        curr = self.root
+        curr = self._root
         rank = 0
 
         while curr:
